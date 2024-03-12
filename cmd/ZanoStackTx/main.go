3faba891056fca3718baa27df5ff6c90e89b112e
kekzploit <kekzploit@skiff.com>
2024-03-12 13:36:20 +0000

    initial commit

---
+++ cmd/ZanoStackTx/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/godarkproject/ZanoStackTx/pkg/storage/mongodb/read"
+	"io"
+	"log"
+	"net/http"
+	"slices"
+)
+
+type GetPaymentsRes struct {
+	Id      int    `json:"id"`
+	Jsonrpc string `json:"jsonrpc"`
+	Result  struct {
+		LastItemIndex int `json:"last_item_index"`
+		Pi            struct {
+			Balance              int64 `json:"balance"`
+			CurentHeight         int   `json:"curent_height"`
+			TransferEntriesCount int   `json:"transfer_entries_count"`
+			TransfersCount       int   `json:"transfers_count"`
+			UnlockedBalance      int   `json:"unlocked_balance"`
+		} `json:"pi"`
+		TotalTransfers int `json:"total_transfers"`
+		Transfers      []struct {
+			Amount          int64  `json:"amount"`
+			Comment         string `json:"comment"`
+			EmployedEntries struct {
+				Receive []struct {
+					Amount  int64  `json:"amount"`
+					AssetId string `json:"asset_id"`
+					Index   int    `json:"index"`
+				} `json:"receive"`
+			} `json:"employed_entries"`
+			Fee             int64    `json:"fee"`
+			Height          int      `json:"height"`
+			IsIncome        bool     `json:"is_income"`
+			IsMining        bool     `json:"is_mining"`
+			IsMixing        bool     `json:"is_mixing"`
+			IsService       bool     `json:"is_service"`
+			PaymentId       string   `json:"payment_id"`
+			RemoteAddresses []string `json:"remote_addresses"`
+			ShowSender      bool     `json:"show_sender"`
+			Subtransfers    []struct {
+				Amount   int64  `json:"amount"`
+				AssetId  string `json:"asset_id"`
+				IsIncome bool   `json:"is_income"`
+			} `json:"subtransfers"`
+			Timestamp             int    `json:"timestamp"`
+			TransferInternalIndex int    `json:"transfer_internal_index"`
+			TxBlobSize            int    `json:"tx_blob_size"`
+			TxHash                string `json:"tx_hash"`
+			TxType                int    `json:"tx_type"`
+			UnlockTime            int    `json:"unlock_time"`
+		} `json:"transfers"`
+	} `json:"result"`
+}
+
+func main() {
+	jsonBody := `
+		{
+		  "jsonrpc": "2.0",
+		  "id": 0,
+		  "method": "get_recent_txs_and_info",
+		  "params": {
+			"offset": 0,
+			"update_provision_info": true,
+			"exclude_mining_txs": true,
+			"count": 100,
+			"order": "FROM_END_TO_BEGIN",
+			"exclude_unconfirmed": true
+		  }
+		}`
+
+	request, err := http.NewRequest("POST", "http://127.0.0.1:11212/json_rpc", bytes.NewBuffer([]byte(jsonBody)))
+	if err != nil {
+		log.Println("error 1 with wallet POST")
+	}
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	client := &http.Client{}
+	res, err := client.Do(request)
+	if err != nil {
+		log.Println("error 2 with wallet POST")
+	}
+	defer func(Body io.ReadCloser) {
+
+	}(res.Body)
+
+	body, _ := io.ReadAll(res.Body)
+	data := GetPaymentsRes{}
+	_ = json.Unmarshal(body, &data)
+
+	for _, transfer := range data.Result.Transfers {
+		confirmations := int64(data.Result.Pi.CurentHeight) - int64(transfer.Height)
+
+		if confirmations >= 10 && transfer.PaymentId != "" {
+
+			// fetch user details
+			user, err := read.FetchUser(transfer.PaymentId)
+			if err != nil {
+				log.Println("user doesnt exist")
+			} else {
+
+				var userTxHashes []string
+				for _, hash := range user.ZanoDeposits {
+					userTxHashes = append(userTxHashes, hash.TxHash)
+				}
+
+				if !slices.Contains(userTxHashes, transfer.TxHash) {
+					//	add tx to user in mongo
+				}
+			}
+		}
+	}
+}
---
+++ pkg/storage/mongodb/read/fetchuser.go
@@ -0,0 +1,72 @@
+package read
+
+import (
+	"context"
+	"errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
+)
+
+type ZanoDeposits struct {
+	TxHash string `bson:"tx_hash"`
+	Amount string `bson:"amount"`
+}
+
+type GameTransactions struct {
+	GameUuid string `bson:"game_uuid"`
+	Amount   string `bson:"amount"`
+}
+
+type Withdrawals struct {
+	TxHash string `bson:"tx_hash"`
+	Amount string `bson:"amount"`
+}
+
+type User struct {
+	ID               primitive.ObjectID `bson:"_id,omitempty"`
+	CreationTime     int64              `bson:"creation_time"`
+	Ip               string             `bson:"ip"`
+	Uuid             string             `bson:"uuid"`
+	Username         string             `bson:"username"`
+	Password         string             `bson:"password"`
+	ZanoAddress      string             `bson:"zano_address"`
+	ZanoPaymentId    string             `bson:"zano_payment_id"`
+	ExternalAddress  string             `bson:"external_address"`
+	ZanoDeposits     []ZanoDeposits     `bson:"zano_deposits"`
+	GameTransactions []GameTransactions `bson:"game_transactions"`
+	Withdrawals      []Withdrawals      `bson:"withdrawals"`
+	Balance          int64              `bson:"balance"`
+}
+
+func FetchUser(paymentId string) (User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	coll := client.Database("zanostack").Collection("users")
+	// Creates a query filter to match documents in which the "name" is
+	// "Bagels N Buns"
+	filter := bson.D{{"zano_payment_id", paymentId}}
+	// Retrieves the first matching document
+	var result User
+	err = coll.FindOne(context.TODO(), filter).Decode(&result)
+	// Prints a message if no documents are matched or if any
+	// other errors occur during the operation
+	if err != nil {
+		if errors.Is(mongo.ErrNoDocuments, err) {
+			return result, err
+		}
+		panic(err)
+	}
+
+	return result, nil
+}
